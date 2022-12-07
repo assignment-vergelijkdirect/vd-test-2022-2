aa96fc3bfbf9b1be38598fe026fcd85383d947d4
José Nigério Evangelista Lima Bezerra <nigerio@nimolabs.net>
2022-12-07 19:25:03 +0100

    veevalidator configured

--- src/components/forms/carForm.vue
+++ src/components/forms/carForm.vue
@@ -3,31 +3,36 @@
     <template #title>Autoverzekering vergelijken</template>
 
     <template #content>
-      <form>
-        <p>Replace me for input fields</p>
+      <Form @submit="onSubmit">
+        <p>Replace me for Field fields</p>
         <!-- LicensePlate -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="text"
             class="form-control"
             id="LicensePlate"
             placeholder="Nummerplaat"
             v-model="carDetails.licenseplate"
+            name="LicensePlate"
+            :rules="validateLicence"
           />
-          {{ carDetails.licenseplate }}
+          <ErrorMessage name="LicensePlate" />
+          <!-- {{ carDetails.licenseplate }} -->
           <label for="LicensePlate">Nummerplaat</label>
         </div>
         <!-- End LicensePlate -->
 
         <!-- Zipcode -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="text"
             class="form-control"
             id="Zipcode"
             placeholder="Postcode"
             v-model="carDetails.zipcode"
+            name="Zipcode"
           />
+          <ErrorMessage name="Zipcode" />
           {{ carDetails.zipcode }}
           <label for="Postcode">Postcode</label>
         </div>
@@ -35,13 +40,15 @@
 
         <!-- Housenumber -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="text"
             class="form-control"
             id="Housenumber"
             placeholder="Huisnummer"
             v-model="carDetails.housenumber"
+            name="Housenumber"
           />
+          <ErrorMessage name="Housenumber" />
           {{ carDetails.housenumber }}
           <label for="Housenumber">Huisnummer</label>
         </div>
@@ -49,13 +56,15 @@
 
         <!-- Housenumber addition -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="text"
             class="form-control"
             id="HouseAdd"
             placeholder="Toevoeging huisnummer"
             v-model="carDetails.houseAdd"
+            name="HouseAdd"
           />
+          <ErrorMessage name="HouseAdd" />
           {{ carDetails.houseAdd }}
           <label for="HouseAdd">Toevoeging huisnummer</label>
         </div>
@@ -63,13 +72,15 @@
 
         <!-- birthdate -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="date"
             class="form-control"
             id="birthdate"
-            placeholder="Toevoeging huisnummer"
+            placeholder="Geboortedatum"
             v-model="carDetails.birthdate"
+            name="birthdate"
           />
+          <ErrorMessage name="birthdate" />
           {{ carDetails.birthdate }}
           <label for="birthdate">Geboortedatum</label>
         </div>
@@ -77,13 +88,15 @@
 
         <!-- ClaimFree years -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="date"
             class="form-control"
             id="ClaimFree"
             placeholder="Claimvrije jaren"
             v-model="carDetails.claimFree"
+            name="ClaimFree"
           />
+          <ErrorMessage name="ClaimFree" />
           {{ carDetails.claimFree }}
           <label for="ClaimFree">Claimvrije jaren</label>
         </div>
@@ -91,24 +104,27 @@
 
         <!-- Kilometrage -->
         <div class="form-floating mb-3">
-          <input
+          <Field
             type="text"
             class="form-control"
             id="Kilometrage"
             placeholder="Kilometerstand"
             v-model="carDetails.kilometrage"
+            name="Kilometrage"
           />
+          <ErrorMessage name="Kilometrage" />
           {{ carDetails.kilometrage }}
           <label for="Kilometrage">Kilometerstand</label>
         </div>
 
         <button type="button" class="btn" @click="onSubmit">Vergelijken</button>
-      </form>
+      </Form>
     </template>
   </simple-card>
 </template>
 
 <script lang="ts">
+import { Form, Field, ErrorMessage } from "vee-validate";
 import CarDetails from "@/models/carDetails.model";
 import { Options, Vue } from "vue-class-component";
 import SimpleCard from "./simpleCard.vue";
@@ -116,6 +132,9 @@ import SimpleCard from "./simpleCard.vue";
 @Options({
   components: {
     SimpleCard,
+    Form,
+    Field,
+    ErrorMessage,
   },
 })
 export default class CarForm extends Vue {
@@ -130,6 +149,20 @@ export default class CarForm extends Vue {
   onSubmit(): void {
     console.log("form submited");
     console.log(this.carDetails);
+  }
+
+  validateLicence(value: string): any {
+    // if the field is empty
+    if (!value) {
+      return "This field is required";
+    }
+    // if the field is not a valid email
+    const regex = /^[A-Z0-9]*$/;
+    if (!regex.test(value)) {
+      return "This field must be a valid format";
+    }
+    // All is good
+    return true;
   }
 
   // validImput(): void {
